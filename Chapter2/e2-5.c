c5325e06112a14043044a9f564a23ffcec9749d1
Himanshu Jadon <96410955+himanshu-957@users.noreply.github.com>
2023-05-20 13:07:32 +0530

    Create e2-5.c

---
+++ Chapter2/e2-5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+void sqeeze(char s1[], char s2[])
+{
+  int flag = 0;
+  int index = 100;
+  int min = index;
+  for(int i = 0; s1[i] != '\0'; i++)
+  {
+    for(int j = 0; s2[j] != '\0'; j++)
+    {
+      if(s1[i] == s2[j])
+      {
+        index = i;
+        flag = 1;
+        break;
+      }
+    }
+    if(index < min)
+    {
+      min = index;
+    }
+  }
+  if(flag == 0)
+  {
+    printf("-1\n");
+  }
+  else
+  {
+    printf("%d",min);
+  }
+}
+
+int main()
+{
+  char s1[10], s2[10];
+  gets(s1);
+  gets(s2);
+  sqeeze(s1,s2);
+  return 0;
+}
