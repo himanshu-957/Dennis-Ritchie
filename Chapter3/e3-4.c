e225c5db2379c7749ed080f9fc3688b6d5e1529c
Himanshu Jadon <96410955+himanshu-957@users.noreply.github.com>
2023-05-31 11:35:52 +0530

    Update e3-4.c

--- Chapter3/e3-4.c
+++ Chapter3/e3-4.c
@@ -1,34 +1,68 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-void reverse(char s[])
-{
+int getwordsize();
+void reverse(char s[]);
+void itoa(int n, char s[]);
+
+int main() {
+  char s[1000];
+  
+  int i = 0;
+  itoa(i, s);
+  printf("%d to string is: %s\n", i, s);
+
+  i = -128;
+  itoa(i, s);
+  printf("%d to string is: %s\n", i, s);
+
+  i = 256;
+  itoa(i, s);
+  printf("%d to string is: %s\n", i, s);
+  
+  i = 1 << (getwordsize() - 1);
+  itoa(i, s);
+  printf("%d to string is: %s\n", i, s);
+}
+
+int getwordsize() {
+  int i = 1;
+  int j = 1;
+  while ((i <<= 1) != 0) {
+    j++;
+  }
+}
+
+void reverse(char s[]) {
   int c, i, j;
-  for (i = 0, j = strlen(s)-1; i < j; i++, j--) 
-  {
+  for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
     c = s[i];
     s[i] = s[j];
     s[j] = c;
   }
 }
 
-void itoa(int n, char s[])
-{
-  int i, sign;
-  if ((sign = n) < 0) /* record sign */
-    n = -n; /* make n positive */
-  i = 0;
-  do { /* generate digits in reverse order */
-    s[i++] = n % 10 + '0'; /* get next digit */
-  } while ((n /= 10) > 0); /* delete it */
-  if (sign < 0)
+void itoa(int n, char s[]) {
+  int i = 0;
+  int sign = n % 10;
+  s[i++] = (sign < 0 ? -sign : sign) + '0';
+
+  n /= 10;
+  if (n < 0) {
+    sign = n;
+    n = -n;
+  }
+
+  while (n > 0) {
+    s[i++] = n % 10 + '0';
+    n /= 10;
+  }
+
+  if (sign < 0) {
     s[i++] = '-';
+  }
+
   s[i] = '\0';
+
   reverse(s);
-}
-int main()
-{
-  char s[100];
-  itoa(10011, s);
-  puts(s);
-  return 0;
 }
